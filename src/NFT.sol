7c22eb890deb160b1ebb8935f48316b44acfed6a
aapsi <aapsikhaira98@gmail.com>
2025-02-23 23:47:57 +0530

    feat: add NFT contract with ERC721 and ERC721Enumerable support

---
+++ src/NFT.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract NFT is ERC721, ERC721Enumerable {}
